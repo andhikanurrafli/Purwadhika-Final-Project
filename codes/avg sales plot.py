7d8bfaddd8892084b8e48a5ad5adf773f6c01d06
Andhika Nurrafli Putra <andhikanurrafli@gmail.com>
2025-02-05 14:22:11 +0700

    Update avg sales plot.py

--- codes/avg sales plot.py
+++ codes/avg sales plot.py
@@ -1,7 +1,7 @@
 avgSales20202022 = dfFiltered2020To2022.groupby('Year')['Sales'].mean()
 avgProfit20202022 = dfFiltered2020To2022.groupby('Year')['Profit'].mean()
 
-
+# perbandingan dengan menggunakan bar plot
 plt.figure(figsize=(10, 5))
 plt.plot(avgSales20202022.index, avgSales20202022.values, marker='o', linestyle='-', color='b', label='Avg Sales')
 
@@ -12,3 +12,28 @@ plt.title('Rata-rata Sales per Tahun 2020 - 2022')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+
+# menggunakan line plot
+
+df.index = pd.to_datetime(df.index)
+
+df['Year'] = df.index.year
+
+avgSalesPerYear = df.groupby('Year')['Sales'].mean()
+
+
+plt.figure(figsize=(10, 5))
+plt.bar(avgSalesPerYear.index, avgSalesPerYear.values, marker='o', linestyle='-', color='b', label='Avg Sales')
+
+
+plt.xlabel('Tahun')
+plt.ylabel('Rata-rata Sales')
+plt.title('Rata-rata Sales per Tahun')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+# bar plot
+
